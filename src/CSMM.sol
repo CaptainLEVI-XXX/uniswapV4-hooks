d6ab263aeca3da90a4aab3128c9961c24c415969
saurabh <saurabhyadav42399@gmail.com>
2025-03-19 05:47:42 +0530

    added liquidity provision in the CSMM hook

--- src/CSMM.sol
+++ src/CSMM.sol
@@ -2,82 +2,68 @@
 pragma solidity 0.8.26;
 
 import {BaseHook} from "v4-periphery/src/utils/BaseHook.sol";
-import {PoolKey} from "v4-core/types/PoolKey.sol";
-import {BalanceDelta} from "v4-core/types/BalanceDelta.sol";
-import {CurrencyLibrary, Currency} from "v4-core/types/Currency.sol";
-import {CurrencySettler} from "@uniswap/v4-core/test/utils/CurrencySettler.sol";
-import {BeforeSwapDelta, toBeforeSwapDelta} from "v4-core/types/BeforeSwapDelta.sol";
 import {IPoolManager} from "v4-core/interfaces/IPoolManager.sol";
 import {Hooks} from "v4-core/libraries/Hooks.sol";
+import {PoolKey} from "v4-core/types/PoolKey.sol";
+import {CurrencyLibrary, Currency} from "v4-core/types/Currency.sol";
+import {CurrencySettler} from "@uniswap/v4-core/test/utils/CurrencySettler.sol";
 
 contract CSMM is BaseHook {
     using CurrencySettler for Currency;
 
-    error InvalidPath();
+    error PathNotAccessible();
 
-   struct CallbackData{
-    uint256 amountEach;
-    Currency token0;
-    Currency token1;
-    address sender;
-   }
+    struct CallbackData {
+        uint256 amountEach;
+        Currency currency0;
+        Currency currency1;
+        address sender;
+    }
 
+    constructor(IPoolManager _manager) BaseHook(_manager) {}
 
-    constructor(IPoolManager _manager) BaseHook(_manager){}
-
-     function getHookPermissions() public pure override returns (Hooks.Permissions memory) {
+    function getHookPermissions() public pure override returns (Hooks.Permissions memory) {
         return Hooks.Permissions({
             beforeInitialize: false,
             afterInitialize: false,
-            beforeAddLiquidity: true,               // don't allow adding liquidity normally
+            beforeAddLiquidity: true,
             beforeRemoveLiquidity: false,
             afterAddLiquidity: false,
             afterRemoveLiquidity: false,
-            beforeSwap: true,                       // change the swap logic to x+y=k
+            beforeSwap: true,
             afterSwap: false,
             beforeDonate: false,
             afterDonate: false,
-            beforeSwapReturnDelta: true,             //Allow before swap to return custom delta
+            beforeSwapReturnDelta: true,
             afterSwapReturnDelta: false,
             afterAddLiquidityReturnDelta: false,
             afterRemoveLiquidityReturnDelta: false
         });
     }
 
-       function _beforeAddLiquidity(address, PoolKey calldata, IPoolManager.ModifyLiquidityParams calldata, bytes calldata)
-        pure
+    function _beforeAddLiquidity(address, PoolKey calldata, IPoolManager.ModifyLiquidityParams calldata, bytes calldata)
         internal
+        pure
         override
         returns (bytes4)
     {
-        revert InvalidPath();
+        revert PathNotAccessible();
     }
 
-     function _beforeSwap(address sender, PoolKey calldata key, IPoolManager.SwapParams calldata params, bytes calldata hookData)
-        internal
-        override
-        returns (bytes4, BeforeSwapDelta, uint24)
-    {
-       /// @dev- todo 
+    function addLiquidity(PoolKey calldata params, uint256 amountEach) external {
+        poolManager.unlock(abi.encode(CallbackData(amountEach, params.currency0, params.currency1, msg.sender)));
     }
 
+    function unlockCallback(bytes calldata data) external onlyPoolManager returns (bytes memory) {
+        CallbackData memory callbackData = abi.decode(data, (CallbackData));
 
-    function addLiquidity(PoolKey calldata key , uint256 amountEach) external {
+        callbackData.currency0.settle(poolManager, callbackData.sender, callbackData.amountEach, false);
 
-        poolManager.unlock(abi.encode(CallbackData(amountEach,key.currency0,key.currency1,msg.sender)));
+        callbackData.currency1.settle(poolManager, callbackData.sender, callbackData.amountEach, false);
 
+        callbackData.currency0.take(poolManager, address(this), callbackData.amountEach, true);
+        callbackData.currency1.take(poolManager, address(this), callbackData.amountEach, true);
+
+        return "";
     }
-
-    function _unlockCallback(bytes calldata data) internal override returns(bytes memory){
-        
-    }
-
-
-
-
-     
-
-
-
-
 }
