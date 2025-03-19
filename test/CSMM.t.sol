9089260b5aabbb983aec4f608c9c885d953b81fa
saurabh <saurabhyadav42399@gmail.com>
2025-03-19 07:56:14 +0530

    added test for CSMM hook

--- test/CSMM.t.sol
+++ test/CSMM.t.sol
@@ -53,4 +53,55 @@ contract TestCSMM is Test, Deployers {
         assertEq(token0ClaimBalance, 1000 ether);
         assertEq(token1ClaimBalance, 1000 ether);
     }
+
+    function test_swap_exactInput_zeroForOne() public {
+        PoolSwapTest.TestSettings memory settings =
+            PoolSwapTest.TestSettings({takeClaims: false, settleUsingBurn: false});
+        // Swap exact input 100 Token A
+
+        uint256 balanceOfTokenABefore = key.currency0.balanceOfSelf();
+        uint256 balanceOfTokenBBefore = key.currency1.balanceOfSelf();
+
+        swapRouter.swap(
+            key,
+            IPoolManager.SwapParams({
+                zeroForOne: true,
+                amountSpecified: -100e18,
+                sqrtPriceLimitX96: TickMath.MIN_SQRT_PRICE + 1
+            }),
+            settings,
+            ZERO_BYTES
+        );
+
+        uint256 balanceOfTokenAAfter = key.currency0.balanceOfSelf();
+        uint256 balanceOfTokenBAfter = key.currency1.balanceOfSelf();
+
+        assertEq(balanceOfTokenBAfter - balanceOfTokenBBefore, 100e18);
+        assertEq(balanceOfTokenABefore - balanceOfTokenAAfter, 100e18);
+    }
+
+    function test_swap_exactOutput_zeroForOne() public {
+        PoolSwapTest.TestSettings memory settings =
+            PoolSwapTest.TestSettings({takeClaims: false, settleUsingBurn: false});
+
+        uint256 balanceOfTokenABefore = key.currency0.balanceOfSelf();
+        uint256 balanceOfTokenBBefore = key.currency1.balanceOfSelf();
+
+        swapRouter.swap(
+            key,
+            IPoolManager.SwapParams({
+                zeroForOne: true,
+                amountSpecified: 100e18,
+                sqrtPriceLimitX96: TickMath.MIN_SQRT_PRICE + 1
+            }),
+            settings,
+            ZERO_BYTES
+        );
+
+        uint256 balanceOfTokenAAfter = key.currency0.balanceOfSelf();
+        uint256 balanceOfTokenBAfter = key.currency1.balanceOfSelf();
+
+        assertEq(balanceOfTokenBAfter - balanceOfTokenBBefore, 100e18);
+        assertEq(balanceOfTokenABefore - balanceOfTokenAAfter, 100e18);
+    }
 }
