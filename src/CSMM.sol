4cffe8f07dd2f075f57761eb78dcbebf98904a7d
saurabh <saurabhyadav42399@gmail.com>
2025-03-19 06:23:38 +0530

    added impl for before swap delta hook

--- src/CSMM.sol
+++ src/CSMM.sol
@@ -7,6 +7,7 @@ import {Hooks} from "v4-core/libraries/Hooks.sol";
 import {PoolKey} from "v4-core/types/PoolKey.sol";
 import {CurrencyLibrary, Currency} from "v4-core/types/Currency.sol";
 import {CurrencySettler} from "@uniswap/v4-core/test/utils/CurrencySettler.sol";
+import {BeforeSwapDelta, toBeforeSwapDelta} from "v4-core/types/BeforeSwapDelta.sol";
 
 contract CSMM is BaseHook {
     using CurrencySettler for Currency;
@@ -65,5 +66,37 @@ contract CSMM is BaseHook {
         callbackData.currency1.take(poolManager, address(this), callbackData.amountEach, true);
 
         return "";
+    }
+
+    function _beforeSwap(
+        address sender,
+        PoolKey calldata key,
+        IPoolManager.SwapParams calldata params,
+        bytes calldata data
+    ) internal virtual override returns (bytes4, BeforeSwapDelta, uint24) {
+        uint256 amountInOutPositive =
+            params.amountSpecified > 0 ? uint256(params.amountSpecified) : uint256(-params.amountSpecified);
+
+        BeforeSwapDelta beforeSwapDelta =
+            toBeforeSwapDelta(int128(-params.amountSpecified), int128(params.amountSpecified));
+
+        if (params.zeroForOne) {
+            //If user is selling Token 0 and buying Token 1
+            // They will be sending Token 0 to the PM, creating a debit of Token 0 in the PM
+            // We will take claim tokens for that Token 0 from the PM and keep it in the hook to create an equivalent credit for ourselves
+
+            key.currency0.take(poolManager, address(this), amountInOutPositive, true);
+
+            // They will be receiving Token 1 from the PM, creating a credit of Token 1 in the PM
+            // We will burn claim tokens for Token 1 from the hook so PM can pay the user
+
+            key.currency1.settle(poolManager, address(this), amountInOutPositive, true);
+        } else {
+            key.currency1.take(poolManager, address(this), amountInOutPositive, true);
+
+            key.currency0.settle(poolManager, address(this), amountInOutPositive, true);
+        }
+
+        return (this.beforeSwap.selector, beforeSwapDelta, 0);
     }
 }
